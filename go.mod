c59a453f004eb989973078ef296e433ff0ca86d4
phpyandong <seiyaz@163.com>
2021-03-20 11:07:45 +0800

    单调钟解决时间回调

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/phpyandong/memorydb
 
 go 1.13
 
-require golang.org/x/tools v0.1.0
+require (
+	golang.org/x/tools v0.1.0
+)
--- uuid/snowflake/snowflake.go
+++ uuid/snowflake/snowflake.go
@@ -5,6 +5,14 @@ import (
 	"time"
 	"fmt"
 )
+//| 1 Bit Unused | 41 Bit Timestamp |  10 Bit NodeID  |   12 Bit Sequence ID |
+//使用默认设置，这允许每个节点ID每毫秒生成4096个唯一ID。
+//https://github.com/bwmarrin/snowflake
+//一个非常简单的Twitter雪花生成器。
+//解析现有雪花ID的方法。
+//将雪花ID转换为其他几种数据类型并返回的方法。
+//JSON Marshal / Unmarshal函数可轻松在JSON API中使用雪花ID。
+//单调时钟计算可防止时钟漂移。
 var (
 	//将纪元设置为2010年11月4日UTC的twitter雪花纪元（以毫秒为单位）
 	Epoch int64 = 1288834974657
@@ -67,19 +75,37 @@ func NewNode(node int64)(*Node,error){
 	n.nodeShift = StepBits
 	var curTime = time.Now()
 	// add time.Duration to curTime to make sure we use the monotonic clock if available
-	//这里可能是 解决时钟回调的问题？
-	n.epoch = curTime.Add( //当前时间加上时间d
+	//这里解决时钟回调的问题
+	//https://www.yuque.com/chao77977/golang/nafo0g
+	//https://zhuanlan.zhihu.com/p/47754783
+	//Wall Clocks，顾名思义，表示墙上挂的钟，在这里表示我们平时理解的时间，
+	// 存储的形式是自 1970 年 1 月 1 日 0 时 0 分 0 秒以来的时间戳，
+	// 当系统和授时服务器进行校准时间时间操作时，
+	// 有可能造成这一秒是2018-1-1 00:00:00，而下一秒变成了2017-12-31 23:59:59的情况。
+	// Monotonic Clocks，意思是单调时间的，所谓单调，就是只会不停的往前增长，
+	// 不受校时操作的影响，这个时间是自进程启动以来的秒数。
+	//如果每隔一秒生成一个Time并打印出来，就会看到如下输出
+	//2018-10-26 14:15:50.306558969 +0800 CST m=+0.000401093
+	//可以看到m=+后面所显示的数字，就是文档中所说的Monotonic Clocks。
+	fmt.Println("时间间隔：",	time.Unix(Epoch/1000,(Epoch%1000) * 1000 * 1000 ).Sub(curTime))
+	n.epoch = curTime.Add( //当前时间加上时间d ,差值是根据m=+0.000401093 来计算的保证了时钟的单调递增
 		time.Unix(Epoch/1000,(Epoch%1000) * 1000 * 1000 ).Sub(curTime), //
 		//根据预设的时间创建一个时间戳，减去当前时间 得到一个差值；如果
 		)
-
+	//fmt.Println("epoch:",n.epoch)
+	//epoch: 2010-11-04 09:42:54.657 +0800 CST m=-327374333.383055884  服务开始启动后，
+	// 利用真实时间的原子钟和epoch的原子钟的差作为当前的时间戳，由于真实时间的单调钟，是单调递增的，
+	// 因此差值就是单调递增的
 	return &n ,nil
 }
 
 func (node *Node)Generate(){
 	node.mutex.Lock()
 	//服务启动的写死了
+	fmt.Println("epoch:",node.epoch)
+
 	now := time.Since(node.epoch).Nanoseconds() /(1000 * 1000)
+	fmt.Println("now:",now)
 	if now == node.time {
 		fmt.Println("83 等于")
 		node.step = (node.step +1) & node.stepMask
